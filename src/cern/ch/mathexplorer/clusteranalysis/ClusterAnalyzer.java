32bfa77e25f7aa5134dbdb1b4013d6fd114430db
Arthur Oviedo <arthur.oviedo@cern.ch>
2014-03-07 18:10:10 +0100

    removed web legacy code

--- src/cern/ch/mathexplorer/clusteranalysis/ClusterAnalyzer.java
+++ src/cern/ch/mathexplorer/clusteranalysis/ClusterAnalyzer.java
@@ -15,14 +15,29 @@ import org.apache.mahout.clustering.iterator.ClusterWritable;
 import org.apache.mahout.common.distance.CosineDistanceMeasure;
 import org.apache.mahout.common.distance.DistanceMeasure;
 
+/**
+ * Clusters created with:
+ * ./mahout kmeans 
+ * -i ../results/vectors 
+ * -o ../results/clustering_kmeans/k256 
+ * -xm sequential 
+ * -k 256 
+ * --clusters ../results/clustering_kmeans/k256 
+ * -x 50 
+ * -cd 0.1 
+ * -cl 
+ * -dm org.apache.mahout.common.distance.CosineDistanceMeasure
+ *
+ */
+
 public class ClusterAnalyzer {
 	static List<Cluster> clusters = new ArrayList<Cluster>();
 
 	public static void main(String args[]) throws Exception {
-		int k = 128;
+		int k = 2;
 		
 		String inputFile = "/home/cern/Desktop/Thesis/sw/mahout-distribution-0.9_2/results/clustering_kmeans/k"+k+""
-				+ "/clusters-10-final/";
+				+ "/clusters-2-final/";
 		for( int i = 0; i<k; i++) {
 			String ending = "part-0";
 			if(i<10){
