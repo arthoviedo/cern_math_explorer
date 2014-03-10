2d7c9d809efe36cf837a2d33f91d75e4dc99e8e1
arthoviedo <arthoviedo@gmail.com>
2014-03-10 19:59:18 +0100

    wrote clusring results work on document

---
+++ src/cern/ch/mathexplorer/mahout/MahoutTasks.java
@@ -0,0 +1,11 @@
+package cern.ch.mathexplorer.mahout;
+
+import javax.security.auth.login.Configuration;
+
+import org.apache.mahout.text.LuceneStorageConfiguration;
+
+public class MahoutTasks {
+	public static void main(String[] args) {
+		LuceneStorageConfiguration storageConfig = new LuceneStorageConfiguration();
+	}
+}
